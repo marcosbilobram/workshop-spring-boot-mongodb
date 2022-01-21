1b9e0a52d26ba47b6942ac83ad830e3b283e3a74
Marcos Henrique <marcosbilobram@hotmail.com>
2022-01-21 10:31:40 -0300

    Atualização de usuário com PUT

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -35,8 +35,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+
 	}
 	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}	
 }
