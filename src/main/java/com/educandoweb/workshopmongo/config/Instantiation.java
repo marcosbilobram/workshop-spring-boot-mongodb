fac0e103bcd0d42e678b14fb13cd32fb89534925
Marcos Henrique <marcosbilobram@hotmail.com>
2022-01-21 11:54:20 -0300

    Referenciando os posts do usuário

--- src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/workshopmongo/config/Instantiation.java
@@ -41,7 +41,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));	
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 	
 }
